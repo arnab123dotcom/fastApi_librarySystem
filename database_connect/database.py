b452a4bee02a0b35a485dd073abce0cbf6eaa5a9
arnab123dotcom <69048126+arnab123dotcom@users.noreply.github.com>
2024-08-03 12:40:57 +0530

    Update database.py

--- database_connect/database.py
+++ database_connect/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-database_url = "postgresql://postgres:arnab123#@localhost:5433/librarysys"
+database_url = "postgresql://postgres:your_password_to_the_postgresql@localhost:5433/librarysys"
 
 engine = create_engine(database_url)
 sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
